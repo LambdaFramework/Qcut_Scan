94f024f223b1f9f1d9c50d83c214d1269d10abd7
SiewYan <siew.yan.hoh@cern.ch>
2019-07-11 23:53:24 +0200

    adjusting to padova lsf

--- makejob.sh
+++ makejob.sh
@@ -1,27 +1,19 @@
 #!/bin/sh
 
-cp -rf cmssw_lsf.job CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
+cp -rf padova_lsf LSF_$1_qcut_$2_$3.job
 rm -rf temp
 
 #$1
-sed s/INPUT1/$1/g CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job > temp
-mv temp CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
+sed s/INPUT1/$1/g LSF_$1_qcut_$2_$3.job > temp
+mv temp LSF_$1_qcut_$2_$3.job
 #$2
-sed s/INPUT2/$2/g CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job > temp
-mv temp CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
+sed s/INPUT2/$2/g LSF_$1_qcut_$2_$3.job > temp
+mv temp LSF_$1_qcut_$2_$3.job
 #$3
-sed s/INPUT3/$3/g CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job > temp
-mv temp CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
-#$4
-sed s/INPUT4/$4/g CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job > temp
-mv temp CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
-
-#$5
-sed s/INPUT5/$5/g CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job > temp
-mv temp CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
+#sed s/INPUT3/$3/g LSF_$1_qcut_$2_$3.job > temp
+#mv temp LSF_$1_qcut_$2_$3.job
 
 ##
-chmod a+x CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
-
-## Send job
-bsub -R "rusage[mem=4000]" -q $6 -o Log_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5 CMSSW_$1_mphi_$3_mchi_$2_mhs_$4_qcut_$5.job
+chmod a+x LSF_$1_qcut_$2_$3.job
+##Send job
+bsub -q $3 -o LSF_$1_qcut_$2_$3.log < LSF_$1_qcut_$2_$3.job
--- scan.sh
+++ scan.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
-LHEDIR=/eos/user/s/shoh/LHE/MonojetMatch1JetsDM ##YOUR LHE LOCATION
-queue=1nd
-ONESAMPLE=1
-ZP=3000
-DM=200
-HS=90
+LHEDIR=./LHE/ ##YOUR LHE LOCATION
+queue=local-cms-short
 
 Scan () {
     echo "SCAN Qcut on one sample: $file"
-    XQCUT=`(grep "xqcut" ${LHEDIR}/$file)`
-    echo ${XQCUT}
-    for qcut in $(seq 20 5 120) #scanning from 20 to 120 GeV in step of 5
+    XQCUT=`(grep "minimum kt jet measure between partons" ${LHEDIR}/$file)`
+    echo LHE:: Xqcut value assigned = ${XQCUT}
+    for qcut in $(seq 20 5 30) #scanning from 20 to 120 GeV in step of 5 #120 
     do
-	echo "./makejob.sh ${model} ${chi} ${phi} ${hs} ${qcut} ${queue}"  
-        ./makejob.sh ${model} ${chi} ${phi} ${hs} ${qcut} ${queue}
+	echo "./makejob.sh ${model} ${qcut} ${queue}"  
+        #./makejob.sh ${model} ${qcut} ${queue}
     done
 }
 
 for file in `(ls ${LHEDIR})`
 do
-    model=`(echo $file | awk -F '_' '{print $1}')`
-    phi=`(echo $file | awk -F '-|_' '{print $4}')`
-    chi=`(echo $file | awk -F '-|_' '{print $8}')`
-    hs=`(echo $file | awk -F '-|_' '{print $6}')`
+    model=`(echo $file | awk -F '_' '{print $1"_"$2}')`
+    Scan
 
-    if [ "$ONESAMPLE" == "1" ];then
-	if [ "$chi" == "$DM" ] && [ "$phi" == "$ZP" ] && [ "$hs" == "$HS" ]; then
-	    Scan
-	fi
-    else
-	echo "Scan All"
-	Scan
-    fi
 done
